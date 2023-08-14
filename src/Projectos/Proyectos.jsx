1411980fbe41f61dd1ebb14f4334b5ea57f2bf28
octavioegeafaure <octavioegeaf@gmail.com>
2023-08-14 11:27:56 -0300

    anulo enrutado modal

--- src/Projectos/Proyectos.jsx
+++ src/Projectos/Proyectos.jsx
@@ -3,11 +3,11 @@ import Modal from "./Modal/Modal"
 import Proyecto from "./Proyecto/Proyecto"
 import { motion, useAnimation, useInView } from "framer-motion";
 import { useEffect, useRef } from "react";
-import imgEasy from "../../dist/ImagenesModal/easyrides.png"
+// import imgEasy from "../../dist/ImagenesModal/easyrides.png"
 const misProyectos = [
         {
             titulo: "EasyRides",
-            src: {imgEasy},
+            src: "",
             color: "orange",
         },
         {
