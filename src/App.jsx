cac3c4ccb60131a2376ba8d6fa7be0a8ff3144bf
octavioegeafaure <octavioegeaf@gmail.com>
2023-08-18 10:21:41 -0300

    cambios en index y 404

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <ProyectoElegidoProvider>
-      <BrowserRouter basename="/portfolio/" >
+      <BrowserRouter>
         <Header />
       <Routes>
         <Route path="/portfolio/" element={<Home />} />
